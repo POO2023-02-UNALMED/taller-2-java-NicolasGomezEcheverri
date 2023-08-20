8b2ca9d99320bd3b91f271d268d3da7439998fbf
Nicolas Gomez Echeverri <nigomeze@unal.edu.co>
2023-08-20 18:22:28 -0500

    correcion Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -12,12 +12,20 @@ public class Auto {
 	
 	//metodos
 	public int cantidadAsientos() {
-		return asientos.length;
+		int cntasientos = 0; 
+		for (Asiento asiento_o : asientos) {
+			if (asiento_o instanceof Asiento) {
+				cntasientos++;
+			}
+		}
+		return cntasientos;
 	}
 	public String verificarIntegridad() {
 		for (Asiento asiento_i : asientos) {
-			if (asiento_i.registro != registro) {
-				return "Las piezas no son originales";
+			if (asiento_i instanceof Asiento) {
+				if (asiento_i.registro != registro) {
+					return "Las piezas no son originales";
+				}
 			}
 		}
 	 if (motor.registro != registro) {
