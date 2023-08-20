1c8ff4b1de54cc02bb53bcac4d381b8290f0a1f1
Nicolas Gomez Echeverri <141961453+NicolasGomezEcheverri@users.noreply.github.com>
2023-08-20 00:13:11 -0500

    Add files via upload

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,29 @@
+package test;
+
+public class Auto {
+	//atributos
+	public String modelo;
+	public int precio;
+	public Asiento[] asientos;
+	public String marca;
+	public Motor motor;
+	public int registro;
+	public static int cantidadCreados;
+	
+	//metodos
+	public int cantidadAsientos() {
+		return this.asientos.length;
+	}
+	public String verificarIntegridad() {
+		for (Asiento asiento_i : asientos) {
+			if (asiento_i.registro != registro) {
+				return "Las piezas no son originales";
+			}
+		}
+	 if (motor.registro != registro) {
+		return "Las piezas no son originales";
+	}
+	return "Auto original";
+	}
+}
+	
